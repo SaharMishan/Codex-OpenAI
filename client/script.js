0228f9dd0fffb7ae78b291fb5a6c00dfc1585653
SaharMishan <saharmishan007@gmail.com>
2022-12-26 21:26:37 +0200

    update the server url

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://codexapp.onrender.com", {
+  const response = await fetch("https://codex-app.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
